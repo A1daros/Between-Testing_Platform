ab2091646245f26adb7b3e0122843827ac0921bf
Anatolii Lutai <toliklutai@gmail.com>
2026-08-09 14:55:56 +0300

    feat: implement 'Try again' and 'Go home' button click handlers using useNavigate

--- src/modules/results/ResultPage.tsx
+++ src/modules/results/ResultPage.tsx
@@ -1,8 +1,9 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 export const ResultPage = () => {
   const location = useLocation();
-
+  const navigate = useNavigate();
+  const { testId } = useParams<{ testId: string }>();
   const { score, total } = location.state;
 
   if (!location.state) {
@@ -16,6 +17,25 @@ export const ResultPage = () => {
       <p>
         Your score is {score} out of {total}!
       </p>
+
+      <div className='buttons'>
+        <button
+          disabled={!testId}
+          onClick={() => {
+            navigate(`/tests/${testId}`);
+          }}
+        >
+          Try again
+        </button>
+
+        <button
+          onClick={() => {
+            navigate('/');
+          }}
+        >
+          Go home
+        </button>
+      </div>
     </div>
   );
 };
