a5d92d3719117bc86dfb4e140b051a63b8abb018
Anatolii Lutai <toliklutai@gmail.com>
2026-08-14 15:29:21 +0300

    feat: create basic Protect Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import { HomePage } from './modules/home/HomePage';
 import { QuizPage } from './modules/quiz';
 import { ResultPage } from './modules/results';
 import './App.css';
 import { AdminPage } from './modules/admin';
 import { UserAnswersDetails } from './modules/admin/components';
-import { HomePage } from './modules/home';
 import { Login } from './modules/authentication/components/login';
 import { Register } from './modules/authentication/components/register';
 import { MyResults } from './modules/results/myResults';
+import { ProtectedRoute } from './routes/ProtectedRoute';
+import { HomePage } from './modules/home';
 
 export const App = () => {
   return (
@@ -16,15 +16,19 @@ export const App = () => {
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/' element={<HomePage />} />
-        <Route path='/tests/:testId' element={<QuizPage />} />
-        <Route path='/tests/:testId/results' element={<ResultPage />} />
-        <Route path='/user/results' element={<MyResults />} />
-        <Route path='/admin/results/:testId' element={<AdminPage />} />
-        <Route
-          path='/admin/result-details/:resultId'
-          element={<UserAnswersDetails />}
-        />
+
+        <Route element={<ProtectedRoute />}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/tests/:testId' element={<QuizPage />} />
+          <Route path='/my-results' element={<MyResults />} />
+
+          <Route path='/tests/:testId/results' element={<ResultPage />} />
+          <Route path='/admin/results/:testId' element={<AdminPage />} />
+          <Route
+            path='/admin/result-details/:resultId'
+            element={<UserAnswersDetails />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
