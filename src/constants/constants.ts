0348f51de9ba263b77d1cafa77cfe87399ba08c0
Anatolii Lutai <toliklutai@gmail.com>
2026-09-01 14:17:29 +0300

    fix: solve path for banners from assets

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,10 @@
+import banner01 from '../assets/banners/banner_01.png';
+import banner02 from '../assets/banners/banner_02.png';
+import banner03 from '../assets/banners/banner_03.png';
+
+
 export const BANNERS = [
-  { id: 1, src: 'src/assets/banners/banner_01.png', alt: 'Learning English' },
-  { id: 2, src: 'src/assets/banners/banner_02.png', alt: 'Learning English' },
-  { id: 3, src: 'src/assets/banners/banner_03.png', alt: 'Learning English' },
+  { id: 1, src: banner01, alt: 'Learning English' },
+  { id: 2, src: banner02, alt: 'Learning English' },
+  { id: 3, src: banner03, alt: 'Learning English' },
 ];
